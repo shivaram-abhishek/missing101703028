bd7136f98a8accdda2a4725b45f0633e8ef62dad
shivaram-abhishek <47098858+shivaram-abhishek@users.noreply.github.com>
2020-02-16 22:33:26 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+from distutils.core import setup
+
+setup(
+
+  name = 'missing101703028',
+
+  packages = ['missing101703028'],
+
+  version = '0.1',
+
+  license='MIT',
+
+  description = 'Missing Data Handler for PyPI',
+
+  author = 'Abhishek Shivaram',
+
+  author_email = 'ashivaram_be17@thapar.com',
+
+  url = 'https://github.com/shivaram-abhishek/missing101703028',
+
+  download_url = 'https://github.com/shivaram-abhishek/missing101703028/archive/0.1.tar.gz',
+
+  keywords = ['Missing', 'Data', 'Handler'],
+
+  install_requires=[
+
+          'numpy',
+
+          'pandas'
+
+      ],
+
+  classifiers=[
+
+    'Development Status :: 3 - Alpha',
+
+    'Intended Audience :: Developers',
+
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3',
+
+    'Programming Language :: Python :: 3.4',
+
+    'Programming Language :: Python :: 3.5',
+
+    'Programming Language :: Python :: 3.6',
+
+  ],
+
+)
